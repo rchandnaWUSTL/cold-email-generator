69902f48f56473a39acdf379930b5967ab0bbc1f
rchandnaWUSTL <45045902+rchandnaWUSTL@users.noreply.github.com>
2022-11-07 11:47:07 -0800

    initial commit with some work and playing around

---
+++ script.js
@@ -0,0 +1,21 @@
+// Path: index.html
+//Make sure to include the script.js file in the index.html file
+
+//make a function that returns an integer
+function add(a, b) {
+    return a + b;
+    }
+
+//make a main function
+function main() {
+    //print the result of the add function
+    }
+
+function formSubmit()
+{
+    //get the input from the user
+    console.log("clicked");
+    var input = document.getElementById('input').value;
+    console.log(input);
+    document.getElementById('output').innerHTML = input;
+}
